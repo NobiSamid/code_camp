f80a14576d65b9db9b8fab2735a8cdc36cdee4b5
NobiSamid <nobisamid@gmail.com>
2025-08-25 21:59:00 +0600

    backend motamuti form e daraise

--- Backend/src/controllers/notesController.js
+++ Backend/src/controllers/notesController.js
@@ -1,16 +1,74 @@
-export function getAllNotes(req, res) {
-  res.status(200).send("Just fetched the notes not really");
+import Note from '../../models/Note.js';
+export async function getAllNotes(req, res) {
+  try {
+    const notes = await Note.find().sort({ createdAt: -1 });
+    res.status(200).json(notes);
+  } catch (error) {
+    console.error('Error fetching notes:', error);
+    res.status(500).json({ message: 'Server error' });
+  }
+}  
+
+export async function getNoteById(req, res) {
+  try{
+    const note = await Note.findById(req.params.id);
+    if(!note){
+      return res.status(404).json({ message: "ei note ta khuje pai nai" });
+    }
+    res.status(200).json(note);
+  }catch(error){
+    console.error('Error fetching note by ID:', error);
+    res.status(500).json({ message: 'Server error' });
+  }
 }
 
-export function createNote(req, res) {
-  res.status(201).json({ message: "Note created successfully" })
+export async function createNote(req, res) {
+  try{
+    const { title, content } = req.body;
+    if(!title || !content){
+      return res.status(400).json({ message: "Title and content are required" });
+    }
+    const note = new Note({ title, content });
+
+    const savedNote = await note.save();
+    res.status(201).json(savedNote);
+  }
+  catch(error){
+    console.error('Error creating note:', error);
+    res.status(500).json({ message: 'Server error' });
+  }
 }
 
-export function updateNote(req, res) {
-  res.status(200).json({ message: "Note updated successfully" })
+export async function updateNote(req, res) {
+  try{
+    const { title, content } = req.body;
+    const updateNote = await Note.findByIdAndUpdate(
+      req.params.id, { title, content }, {new: true}
+    );
+    if(!updateNote){
+      return res.status(404).json({ message: "Note not found" });
+    } 
+
+    res.status(200).json(updateNote)
+  }catch(error){
+    console.error('Error updating note:', error);
+    res.status(500).json({ message: 'Server error' });
+  }
 }
 
-export function deleteNote(req, res) {
-  res.status(200).json({ message: "Note deleted successfully" })
+export async function deleteNote(req, res){
+  try{
+    const { title, content } = req.body;
+    const deletedNote = await Note.findByIdAndDelete(req.params.id, { title, content }, {new: true}
+    );
+    if(!deletedNote){
+      return res.status(404).json({ message: "delete je korbo kichu e to khuje pai na" });
+    }
+    res.status(200).json({ message: " de lit kore disi" });
+  }catch(error){
+    console.error("Error deleting note:", error);
+    res.status(500).json({ message: "delet er somoy Server error hoise" });
+  }
 }
+
 
